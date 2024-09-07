ec3fb65f410db20814aaf18b4bb5df4e6b17fb1e
Adigunaguna <bgsputraadiguna@gmail.com>
2024-09-07 22:43:57 +0800

    Update startdebian.sh

--- scripts/proot_debian/startdebian.sh
+++ scripts/proot_debian/startdebian.sh
@@ -8,8 +8,15 @@ pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth
 
 virgl_test_server_android --angle-gl & > /dev/null 2>&1
 sleep 1
-XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :1.0 &
-sleep 1
+
+# Prepare termux-x11 session
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 >/dev/null &
+
+# Wait a bit until termux-x11 gets started.
+sleep 3
+
+# Launch Termux X11 main activity
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
 sleep 1
 DISPLAY=:1.0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session xfce4-session & > /dev/null 2>&1
